81011330d0ba94dd22f8e7a13221bb2ff0462315
Jirawat A <tao.dekdee2536@gmail.com>
2019-05-17 17:42:02 +0700

    [FIX][12.0] partner_shipper

--- partner_shipper/__manifest__.py
+++ partner_shipper/__manifest__.py
@@ -1,3 +1,5 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Partner Shipper',
     'summary': 'Set on partners shipper for delivery goods',
